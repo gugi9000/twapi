d861993e37337d067e7c19398eeeb875908edbeb
Bjarke Sørensen <bs@wasd.dk>
2017-02-15 13:40:07 +0100

    Sort output
    New version of tiberius from github

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,25 @@ use tiberius::stmt::ResultStreamExt;
 
 fn main() {
     let mut lp = Core::new().unwrap();
-    let connect_string = "Server=tcp:localhost,49800;integratedSecurity=true;Database=RIS;".to_owned();
+    let connect_string = "server=tcp:s005,51024;integratedSecurity=true;Database=RIS;".to_owned();
     let query = "select 
-		Sum(ServiceCaseTime.TimeUsed) as Total , SUBSTRING(Customer.Name, 1,21) AS Kunde
+		Sum(ServiceCaseTime.TimeUsed) as Total
+      , SUBSTRING(Customer.Name, 1,21) AS Kunde
 		from [ServiceCaseTime]
-		join [ServiceCase] on (ServiceCaseTime.ServiceCaseId = ServiceCase.Id)
-		join [Customer] on (ServiceCase.CustomerId = Customer.Id)
+    		join [ServiceCase] on (ServiceCaseTime.ServiceCaseId = ServiceCase.Id)
+		    join [Customer] on (ServiceCase.CustomerId = Customer.Id)
 		where [Done] > '2017-02-01'
 		GROUP BY SUBSTRING(Customer.Name, 1,21)
-		--Order By Total DESC";
+		Order By Total DESC";
    let future = SqlConnection::connect(lp.handle(), connect_string.as_str()).and_then(|conn| {
+       println!("{{\"kundetimer\": [");
        conn.simple_query(query).for_each_row(|row| {
            let kunde: &str = row.get("Kunde");
-           let hours: f64 = row.get("Total");
-           println!("\"{}\": {}", kunde, hours);
+           let timer: f64 = row.get("Total");
+           println!("    {{\"{}\":\t{}}},", kunde, timer);
            Ok(())
        })
    });
    lp.run(future).unwrap();
+   println!("]}}");
 }
